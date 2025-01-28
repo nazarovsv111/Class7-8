ac9abc3b9a4828a943dcb7acf025a659d40eb262
nazarovsv111 <vnazarov433@gmail.com>
2025-01-28 16:35:46 +0100

    Initial commit with Python code

---
+++ Practice7class.py
@@ -0,0 +1,15 @@
+from mimetypes import inited
+
+sum = 0
+for x in range(1, 101):
+    sum += x
+print(sum)
+
+
+for i in range(1, 11):
+    for j in range(1, 11):
+        print(f"{i} X {j} = {i*j}")
+    print()
+
+
+
